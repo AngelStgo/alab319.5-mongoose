01f708a8bd90439db2a68dfc687147272006d6ce
AngelStgo <euangel.stgo@gmail.com>
2025-03-18 17:40:40 -0400

    grades schema edit, still working on

--- models/Grades.js
+++ models/Grades.js
@@ -1,7 +1,34 @@
+import mongoose from "mongoose";
+// const mongoose = require("mongoose");
+
+
+// const scoreScheme = new mongoose.Schema({
+//     score: {
+//         type: [Number]
+//     }
+// });
+
 
 const gradesSchemma = new mongoose.Schema({
-    scores: [Number],
-    class_id: Number,
-    learner_id: Number,
-    require: true
+    // scores have an array of class name plus numbered score per each class.
+    score: [
+        {
+            type:{
+                type: String
+            },
+            score: {
+                type: Number
+            } 
+        }
+    ],
+    class_id: {
+        type: Number,
+        require: true
+    },
+    learner_id: {
+        type: Number,
+        require: true
+    },
 });
+
+// export default mongoose.model();
